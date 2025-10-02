7c7555884c9085df6277b7730f3f90b61cd94886
robrvs <39736736+robrvs@users.noreply.github.com>
2025-10-02 02:13:31 -0400

    Create q01.py

---
+++ q01.py
@@ -0,0 +1,6 @@
+home  = int(input("Digite quantas pessoas moram em sua casa: "))
+if (home % 2) == 0:
+ print("O número de pessoas na sua casa é PAR.")
+else:
+    print("O número de pessoas na sua casa é ÍMPAR.")
+    
