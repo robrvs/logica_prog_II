faa016b85938d53639081773b10ad8a53af30718
robrvs <39736736+robrvs@users.noreply.github.com>
2025-10-02 02:17:36 -0400

    Create q02.py

---
+++ q02.py
@@ -0,0 +1,7 @@
+page = int(input("Digite o número da páginas lidas: "))
+if page > 500:
+ print("O número de páginas lidas é MAIOR que 500.")
+elif page < 500:
+    print("O número de páginas lidas é MENOR que 500.")
+else:
+    print("O número de páginas lidas é IGUAL a 500.")   
